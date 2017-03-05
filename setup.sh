4b29f0422f23b7ef7d4ccc2a252d8282c918c264
tezcatli <jean.laurent.1@gmail.com>
2017-03-05 01:34:18 +0100

    initial import

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+YOCTO_VERSION=morty
+
+
+git clone -b ${YOCTO_VERSION} git://git.yoctoproject.org/poky.git
+
+ln -s yocto-build poky/build
+
+cd poky
+
+git clone -b ${YOCTO_VERSION} http://git.openembedded.org/meta-openembedded
+git clone	git://git.yoctoproject.org/meta-fsl-arm.git 
+git clone https://github.com/tezcatli/meta-moonlight.git
+
+
